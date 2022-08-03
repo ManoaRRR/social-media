4e44b6aa624ed36614c46716aea9941ea0841171
ManoaRRR <hei.manoa.2@gmail.com>
2022-08-03 15:00:23 +0000

    update project

--- fakerr.js
+++
@@ -1,7 +0,0 @@
-console.log("dd");
-function WriteToFile() {
-    var fso = new ActiveXObject("Scripting.FileSystemObject");
-    var s = fso.CreateTextFile("C:\Users\RAMBOLANIVO\Desktop\social-media\aaa.txt", true);
-    s.WriteLine('*****test****');
-    s.Close();
-}
--- mocks/3_mockPost.sql
+++ mocks/3_mockPost.sql
@@ -2,13 +2,13 @@
 INSERT INTO post
     (posting_date, posting_time, post_content, id_account) 
 VALUES 
-    (current_date + interval '1 days',now() + interval '1 minutes','Eum rerum sapiente sint.',97205),
-    (current_date + interval '2 days',now() + interval '2 minutes','Ea ut dolores commodi sit quibusdam quod quia.',95291),
+    (current_date + interval '1 days',now() + interval '1 minutes','Eum rerum sapiente sint.',24987),
+    (current_date + interval '2 days',now() + interval '2 minutes','Ea ut dolores commodi sit quibusdam quod quia.',25291),
     (current_date + interval '3 days',now() + interval '3 minutes','Nam assumenda quia.',9155),
-    (current_date + interval '4 days',now() + interval '4 minutes','Et ut aut quasi quibusdam in distinctio et consequatur.',130577),
-    (current_date + interval '5 days',now() + interval '5 minutes','Nam nihil ducimus corrupti laboriosam consequatur.',206352),
-    (current_date + interval '6 days',now() + interval '6 minutes','Voluptatem et eum iste tempora eos incidunt a nobis cumque.',163700),
-    (current_date + interval '7 days',now() + interval '7 minutes','Qui laboriosam aliquam.',78479),
-    (current_date + interval '8 days',now() + interval '8 minutes','Quidem maxime sint.',111111),
-    (current_date + interval '9 days',now() + interval '9 minutes','Ea sunt ut vitae quo quidem.',196087),
-    (current_date + interval '10 days',now() + interval '10 minutes','Illo vitae maxime minus itaque inventore ea quos tenetur.',201926);
+    (current_date + interval '4 days',now() + interval '4 minutes','Et ut aut quasi quibusdam in distinctio et consequatur.',43577),
+    (current_date + interval '5 days',now() + interval '5 minutes','Nam nihil ducimus corrupti laboriosam consequatur.',20352),
+    (current_date + interval '6 days',now() + interval '6 minutes','Voluptatem et eum iste tempora eos incidunt a nobis cumque.',13700),
+    (current_date + interval '7 days',now() + interval '7 minutes','Qui laboriosam aliquam.',7479),
+    (current_date + interval '8 days',now() + interval '8 minutes','Quidem maxime sint.',11211),
+    (current_date + interval '9 days',now() + interval '9 minutes','Ea sunt ut vitae quo quidem.',19087),
+    (current_date + interval '10 days',now() + interval '10 minutes','Illo vitae maxime minus itaque inventore ea quos tenetur.',21926);
